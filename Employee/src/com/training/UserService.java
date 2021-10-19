95358431effe083d78a48f8d9b248e7c2e8dbba6
44740 <44740@DESKTOP-EC0PRJ5>
2021-10-19 14:06:02 +0530

    Update

---
+++ Employee/src/com/training/UserService.java
@@ -0,0 +1,13 @@
+package com.training;
+
+public class UserService {
+	public static void main(String[] args) {
+		UserMethods usersMethods=new UserMethods();
+		usersMethods.addUser();
+		usersMethods.displayAllEmployees();
+		usersMethods.delUser();
+		usersMethods.displayAllEmployees();
+		
+	}
+
+}
